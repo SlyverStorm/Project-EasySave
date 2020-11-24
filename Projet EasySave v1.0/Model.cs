cc12860b3cb17917ecd34a05fdd86fca71c91003
Hakaiju <44494513+Hakaiju@users.noreply.github.com>
2020-11-24 10:48:21 +0100

    Add a log file and can write a single line on it

--- Projet EasySave v1.0/Model.cs
+++ Projet EasySave v1.0/Model.cs
@@ -1,11 +1,14 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
 
 namespace Projet_EasySave_v1._0
 {
     class Model
     {
+        private string jsonLogLine;
 
         public Model()
         {
@@ -43,7 +46,33 @@ namespace Projet_EasySave_v1._0
 
 
 
+        public void createLogLine()
+        {
+            // Translate the log line in a Json format
+            jsonLogLine = JsonConvert.SerializeObject("test");
+            Console.WriteLine(jsonLogLine);
 
+            // call a fonction to write the line in a file
+            addLogLine(); 
+        }
+
+
+        public void addLogLine()
+        {
+           JsonSerializer serializer = new JsonSerializer();
+
+
+            // Write the log line in the log file
+            using (var streamWriter = new StreamWriter("data.json"))
+            {
+                using (var jsonWriter = new JsonTextWriter(streamWriter))
+                {
+                    jsonWriter.Formatting = Formatting.None;
+                    serializer.Serialize(jsonWriter, JsonConvert.DeserializeObject(jsonLogLine));
+                    Console.WriteLine("Record log OK");
+                }
+            }
+        }
 
     }
 }
