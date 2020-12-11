10ef65adc85af0a8f579d6494b7c607f7b38b33c
SlyverStorm <44494841+SlyverStorm@users.noreply.github.com>
2020-12-11 17:03:51 +0100

    Init Model3.0 branch

--- EasySave 2.0/model/language/ILanguage.cs
+++ EasySave 2.0/model/language/ILanguage.cs
@@ -4,6 +4,9 @@ using System.Text;
 
 namespace EasySave_2._0
 {
+    /// <summary>
+    /// Language Interface
+    /// </summary>
     interface ILanguage
     {
         public string MainTitle { get; }
